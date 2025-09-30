4119ceafc050072818a7d31b3d9bfde09e63ce59
opilane <opilane@HP-CZC2349HTC>
2025-09-30 14:51:05 +0300

    muuta

--- Controllers/PrimitiividController.cs
+++ Controllers/PrimitiividController.cs
@@ -3,42 +3,47 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace veeb_TARpv23.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
+    [Route("api/[controller]")] // Marsruutimise atribuut: aadress algab api/Primitiivid
+    [ApiController] // Näitab, et tegemist on API kontrolleriga
     public class PrimitiividController : ControllerBase
     {
-        // GET: primitiivid/hello-world
-        [HttpGet("hello-world")]
+        // GET: api/primitiivid/hello-world - Marsruut
+        [HttpGet("hello-world")] 
         public string HelloWorld()
         {
+            // Tagastab stringi „Hello world“ koos praeguse kuupäeva ja kellaajaga
             return "Hello world at " + DateTime.Now;
         }
 
-        // GET: primitiivid/hello-variable/mari
+        // GET: api/primitiivid/hello-variable/mari  - Marsruut
         [HttpGet("hello-variable/{nimi}")]
         public string HelloVariable(string nimi)
         {
+            // Tagastab tervituse koos edastatud nimega
             return "Hello " + nimi;
         }
 
-        // GET: primitiivid/add/5/6
+        // GET: api/primitiivid/add/5/6   - Marsruut
         [HttpGet("add/{nr1}/{nr2}")]
         public int AddNumbers(int nr1, int nr2)
         {
+            // Liidab kaks arvu ja tagastab tulemuse
             return nr1 + nr2;
         }
 
-        // GET: primitiivid/multiply/5/6
+        // GET: api/primitiivid/multiply/5/6 - Marsruut
         [HttpGet("multiply/{nr1}/{nr2}")]
         public int Multiply(int nr1, int nr2)
         {
+            // Korrutab kaks arvu ja tagastab tulemuse
             return nr1 * nr2;
         }
 
-        // GET: primitiivid/do-logs/5
+        // GET: api/primitiivid/do-logs/5 - Marsruut
         [HttpGet("do-logs/{arv}")]
         public void DoLogs(int arv)
         {
+            // Väljastab konsooli määratud arvu sõnumeid
             for (int i = 0; i < arv; i++)
             {
                 Console.WriteLine("See on logi nr " + i);
--- Controllers/ToodeController.cs
+++ Controllers/ToodeController.cs
@@ -4,24 +4,28 @@ using veeb_TARpv23.models;
 
 namespace veeb_TARpv23.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
+    [Route("api/[controller]")] // Marsruutimise atribuut: aadress algab api/Toode
+    [ApiController] // Näitab, et tegemist on API kontrolleriga
     public class ToodeController : ControllerBase
     {
+        // Toote staatiline objekt (ID, name, price, isActive)
         private static Toode _toode = new Toode(1, "Koola", 1.5, true);
 
-        // GET: toode
+        // GET: api/toode - Marsruut
         [HttpGet]
         public Toode GetToode()
         {
+            // Tagastab praeguse toote objekti
             return _toode;
         }
 
-        // GET: toode/suurenda-hinda
+        // GET: api/toode/suurenda-hinda - Marsruut
         [HttpGet("suurenda-hinda")]
         public Toode SuurendaHinda()
         {
+            // Suurendab toote hinda 1 võrra
             _toode.Price = _toode.Price + 1;
+            // Tagastab uuendatud toote
             return _toode;
         }
     }
